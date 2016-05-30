b8030efe4a03ad82f95ce91290d56d26249be949
Anna Concepcion <anna.concepcion@gmail.com>
2016-05-30 00:53:36 -0400

    Adds baby cheese wheel

--- client/assets/js/scripts.js
+++ client/assets/js/scripts.js
@@ -4,6 +4,11 @@ $(function(){
   $('#title').css({
     'margin-top': '0%'
     });
+
+  $('#cheese-img').css({
+    'opacity': '0'
+    });
+
   setTimeout(function(){
     titleSlide()
   }, 1800);
@@ -13,4 +18,9 @@ function titleSlide(){
   $('#title').animate({
     'margin-top': '-100%'
   }, 2500);
+
+  $('#cheese-img').animate({
+    'opacity': '0.5'
+  }, 2500);
+
 };
