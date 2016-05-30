2de14e23d0f680ec2713636ee6e97888429c814c
Anna Concepcion <anna.concepcion@gmail.com>
2016-05-30 01:09:57 -0400

    Updates title spin in

--- client/assets/js/scripts.js
+++ client/assets/js/scripts.js
@@ -11,16 +11,21 @@ $(function(){
 
   setTimeout(function(){
     titleSlide()
+  }, 1800, "linear");
+  setTimeout(function(){
+    cheeseFade()
   }, 1800);
+
 });
 
 function titleSlide(){
   $('#title').animate({
     'margin-top': '-100%'
   }, 2500);
+};
 
+function cheeseFade(){
   $('#cheese-img').animate({
     'opacity': '0.5'
   }, 2500);
-
 };
